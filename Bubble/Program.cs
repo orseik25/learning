0031862d6b0f6ada5c9d46eb548e25aae4f7c2c6
Vladik <orseik25@gmail.com>
2019-04-02 14:25:35 +0300

    Теперь работает, забыл просто сброс индекса j=i+1

--- Bubble/Program.cs
+++ Bubble/Program.cs
@@ -7,18 +7,20 @@ namespace Bubble
         static void Main(string[] args)
         {
             int i=0;
-            int j=0;
+            int j=1;
             int[] massive = new int [6] {2,1,4,2,445,1};
             while(i!=massive.Length){
+                j=i+1;
                 while(j!=massive.Length){
                                 if (massive[j]>massive[i])
                                                 {
-                                                    //Console.WriteLine("n1={0}",massive[n+1]);
-                                                    //Console.WriteLine("n={0}",massive[n]);
+                                                    //Console.WriteLine("n1={0}",massive[j]);
+                                                    //Console.WriteLine("n={0}",massive[i]);
                                                     massive[j]=massive[i]+massive[j];//Сначала напишем алгоритм замены так как он сейчас на доске
                                                     massive[i]=massive[j]+massive[i];
                                                     massive[j]=massive[i]-massive[j];
                                                     massive[i]=massive[i]-(massive[j]*2);
+                                                    j=i+1;
                                                 }
                                             j++;
                                         }
