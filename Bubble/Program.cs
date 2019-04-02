a857dea9b782434ad600205abcd549d79e597fa3
Vladik <orseik25@gmail.com>
2019-04-02 13:23:17 +0300

    намудил с циклами последнее значение неправильно

--- Bubble/Program.cs
+++ Bubble/Program.cs
@@ -6,30 +6,29 @@ namespace Bubble
     {
         static void Main(string[] args)
         {
-            int n=0;
-            int n1=0; //Костыль пока тут нет кортежа
-            int s=0;
             int i=0;
-
-            while (s=i)
-            {
-                if (n1>n)
-                {
-                    s++;
-                }
-                    else
-                    {
-                    n1=n+n1;//Сначала напишем алгоритм замены так как он сейчас на доске
-                    n=n1+n;
-                    n1=n1-n;
-                    n=n-(n1*2);
+            int j=0;
+            int[] massive = new int [6] {2,1,4,2,445,1};
+            while(i!=massive.Length){
+                while(j!=massive.Length){
+                                if (massive[j]>massive[i])
+                                                {
+                                                    //Console.WriteLine("n1={0}",massive[n+1]);
+                                                    //Console.WriteLine("n={0}",massive[n]);
+                                                    massive[j]=massive[i]+massive[j];//Сначала напишем алгоритм замены так как он сейчас на доске
+                                                    massive[i]=massive[j]+massive[i];
+                                                    massive[j]=massive[i]-massive[j];
+                                                    massive[i]=massive[i]-(massive[j]*2);
+                                                }
+                                            j++;
+                                        }
+                        i++;
                     }
+            foreach(int vivod in massive)
+            {
+                Console.WriteLine(vivod);
             }
-            
-            
-            
-            
-            Console.WriteLine("Hello World!"); 
+            //Console.WriteLine("Hello World!"); 
         }
     }
 }
