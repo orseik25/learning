490b0ac172825c2a65697639f2be291eda3db740
Laptop <orseik25@gmail.com>
2019-04-23 12:59:41 +0300

    вVse oko avtokod gavno

---
+++ insertion short/Peremeshenie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Peremeshenie
+{
+	public Peremeshenie()
+	{
+        void Перемещение(/*Вход, arr[], VstavkaVRazriv, MestoRazriva, Выход, arr[]*/)
+        {
+            // item 120
+            /*arr[]
+            VstavkaVRazriv
+            MestoRazriva*/
+            // item 100
+            String newarr = null;
+            foreach (int i in arr)
+            {
+                // item 1030001
+                if (switch == i = MestoRazriv;) {
+            // item 113
+            newarr = +(Convert.ToString(arr[VstavkaVRazriv])) + " " + (Convert.ToString(arr[i]));
+        } else
+        {
+            // item 1030002
+            if (switch == i = VstavkaVRazriv;) {
+
+            } else
+            {
+                // item 1030003
+                if (switch == default) {
+
+                } else
+                {
+                    // item 1030004
+                    throw new InvalidOperationException("Not expected:  " + switch.ToString());
+                }
+                // item 102
+                newarr = +(Convert.ToString(arr[i])) + " ";
+            }
+        }
+    }
+    // item 196
+    Переработка
+    // item 122
+    //newarr[]
+}
+	}
+}
--- insertion short/insertionshort/Program.cs
+++ insertion short/insertionshort/Program.cs
@@ -6,7 +6,29 @@ namespace insertionshort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int[] arr = { 4, 6, 8, 4, 2 };// Хочу еще добавить сюда ввод с клавы через пробелы
+            int j = 0;
+            int VstavkaVRazriv = 0;
+            int MestoRazriva = 0;
+            foreach (int i in arr)
+            {
+                for(j = i - 1;j = !0; j--)
+                {
+                    if (arr[j] >= arr[i])
+                    {
+                        if (arr[j - 1] >= arr[i])
+                        {
+
+                        }
+                        else
+                        {
+                            VstavkaVRazriv = i;
+                            MestoRazriva = j;
+                            //Здесь должен быть переход в класс премещение с передачей массива разрыва и вставляемого элемента
+                        }
+                    }
+                }
+            }
+        }//Сюда надо будет добавить вывод массива
     }
 }
