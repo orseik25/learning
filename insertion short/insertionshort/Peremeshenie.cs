23450fc001cf42d68cfd96278d017fdad5ddd40e
Laptop <orseik25@gmail.com>
2019-04-24 13:10:01 +0300

    Получилось, но еще не готово

---
+++ insertion short/insertionshort/Peremeshenie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Peremeshenie
+{
+    public int[] Perem(int[] arr, ref int VstavkaVRazriv, ref int MestoRazriva)
+	{
+        //void Перемещение(/*Вход, arr[], VstavkaVRazriv, MestoRazriva, Выход, arr[]*/)
+            // item 120
+            /*arr[]
+            VstavkaVRazriv
+            MestoRazriva*/
+            // item 100
+            String newarr = null;
+            for(int i=0;i<=arr.Length;i++)
+            {
+            // item 1030001
+            if (i == MestoRazriva)
+            {
+                newarr += (Convert.ToString(arr[VstavkaVRazriv])) + " " + (Convert.ToString(arr[i]));
+            } else {
+                    if (i == VstavkaVRazriv) { }
+                     else
+                     {
+                       newarr += (Convert.ToString(arr[i])) + " ";
+                     }
+                   }
+            }
+        Pererabotka Raz = new Per(newarr);
+        return arr;
+        // item 122
+        //newarr[]
+    }
+	} // Вернем массив
--- insertion short/insertionshort/Program.cs
+++ insertion short/insertionshort/Program.cs
@@ -12,19 +12,16 @@ namespace insertionshort
             int MestoRazriva = 0;
             foreach (int i in arr)
             {
-                for(j = i - 1;j = !0; j--)
+                for(j = i - 1;j != 0; j--)
                 {
                     if (arr[j] >= arr[i])
                     {
-                        if (arr[j - 1] >= arr[i])
-                        {
-
-                        }
+                        if (arr[j - 1] >= arr[i]) { }
                         else
                         {
                             VstavkaVRazriv = i;
                             MestoRazriva = j;
-                            //Здесь должен быть переход в класс премещение с передачей массива разрыва и вставляемого элемента
+                            Peremeshenie peremeshenie = new Peremeshenie(arr, ref VstavkaVRazriv, ref MestoRazriva);//Здесь должен быть переход в класс премещение с передачей массива разрыва и вставляемого элемента
                         }
                     }
                 }
