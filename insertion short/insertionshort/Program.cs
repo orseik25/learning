bdfb3f1e8042ea896bd9cfa189c17d68f1f27af2
Laptop <orseik25@gmail.com>
2019-04-30 09:23:16 +0300

    заработало, теперь можно смотреть отгадку в книге

--- insertion short/insertionshort/Program.cs
+++ insertion short/insertionshort/Program.cs
@@ -7,17 +7,24 @@ namespace insertionshort
         static void Main(string[] args)
         {
             int[] arr = { 4, 6, 8, 4, 2 };// Хочу еще добавить сюда ввод с клавы через пробел
-            for (int i = 2; i <= arr.Length; i++)
+            for (int i = 2; i <= (arr.Length-1); i++)//Ебаная сука Вес масива это колл во элементов, а не максимальный индекс сцуко бля!!!!!!
             {
                 for (int j = i - 1; j >= 0; j--)
                 {
                     if (arr[i]<=arr[j])
                     {
+                        if (j==0)
+                        {
+                            Zam(i, j, ref arr);
+                            break;
+                           
+                        }
                         if (arr[i] > arr[j - 1])
                         {
-                            Zam(i,j,ref arr);
+                            Zam(i, j, ref arr);
                             break;
                         }
+
                     }
                     else
                     {
